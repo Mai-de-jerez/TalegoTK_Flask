b48ba3d24fd510230e06f1b8be2bca3d2265e0c1
Mai <mainen1985@gmail.com>
2025-09-18 12:37:04 +0200

    Actualización del proyecto con cambios locales

--- app.py
+++ app.py
@@ -27,7 +27,6 @@ def index():
 from blueprints.users.routes import users_bp
 from blueprints.books.routes import books_bp
 from blueprints.loans.routes import loans_bp
-from blueprints.export.routes import export_bp
 
 # Crear la instancia de la aplicación Flask
 app = Flask(__name__)
@@ -62,6 +61,6 @@ app.register_blueprint(main_bp)
 app.register_blueprint(users_bp, url_prefix='/usuarios')
 app.register_blueprint(books_bp, url_prefix='/libros')
 app.register_blueprint(loans_bp, url_prefix='/prestamos')
-app.register_blueprint(export_bp, url_prefix='/admin')
 
 logging.info("Aplicación Flask configurada y Blueprints registrados.")
+
